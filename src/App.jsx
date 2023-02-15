3125f54f59698b9a342a1010f84a8fda3e7ea8bf
David Mendoza <davidleomendoza300@gmail.com>
2023-02-15 16:09:46 -0500

    Agregar todos los componentes a mi App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Experience from './components/Experience/Experience.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Header from './components/Header/Header.jsx';
+import Nav from './components/Nav/Nav.jsx';
+import Portafolio from './components/Portafolio/Portafolio.jsx';
+import Services from './components/Services/Services.jsx';
+import Testimonials from './components/Testimonials/Testimonials.jsx';
 const App = () => {
     return (
-        <div>App</div>
+        <div><Header/></div>
     )
 } 
 export default App;
