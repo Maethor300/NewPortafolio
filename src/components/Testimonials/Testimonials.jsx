b9575b880d04bc50d753a2c1106f403a5252db92
David Mendoza <davidleomendoza300@gmail.com>
2023-02-17 10:19:11 -0500

    Finished portafolio

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,17 @@
 import React from "react"
 import './Testimonials.css'
+import AVTR1 from '../../assets/'
 const Testimonials = () => {
     return (
-        <section id='testimonials'>Testimonials</section>
+        <section id='testimonials'>
+            <h5>Review From Clients</h5>
+            <h2>Testimonials</h2>
+            <div className="container testimonials__container">
+                <article className="testimonial">
+
+                </article>
+            </div>
+        </section>
     )
 }
 export default Testimonials;
